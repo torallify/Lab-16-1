cb8f0f8479a8f9d31d40d7d81363abb77feb67fb
Lucero Toral <lucerotoral@quickenloans.com>
2020-04-27 16:14:51 -0400

    basic register model without JavaScript

---
+++ Lab_16-1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab_16_1.Models;
+
+namespace Lab_16_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            User u = new User();
+            return View(u);
+        }
+
+        [HttpPost]
+        public IActionResult Register(User u)
+        {
+            // validate server-side (back-end)
+            // can use data annotations etc in model class
+            // this is our final and safest check on the data
+
+            //the client-side validation tries to limit unnecessary server load
+            //and the time the user has to wiat for response
+
+            // put info into the database
+            return View("RegisterSuccess", u);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
